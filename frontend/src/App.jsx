cc76e655eb2f55ab2abad378fbc5db1c15d58130
jarchit27 <j.archit134@gmail.com>
2026-08-07 19:37:09 +0530

    feat: implement main application layout and chat page routing structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,18 +14,17 @@ function App() {
     checkAuth();
   }, [checkAuth])
 
-  console.log({ authUser });
 
   if (isCheckingAuth) return <PageLoader />
 
   return (
-    <div className="h-screen bg-slate-900 flex items-center justify-center overflow-hidden relative">
+    <div className="h-screen bg-slate-900 overflow-hidden relative">
       {/* DECORATORS - GRID BG & GLOW SHAPES */}
       <div className="absolute inset-0 pointer-events-none bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
       <div className="absolute top-0 left-0 w-1/2 aspect-square max-w-[400px] pointer-events-none bg-pink-500 opacity-20 blur-[100px] -translate-x-1/2 -translate-y-1/2" />
       <div className="absolute bottom-0 right-0 w-1/2 aspect-square max-w-[400px] pointer-events-none bg-cyan-500 opacity-20 blur-[100px] translate-x-1/3 translate-y-1/3" />
 
-      <div className="w-full z-10">
+      <div className="w-full h-full z-10 flex items-center justify-center">
         <Routes>
           <Route path="/" element={authUser ? <ChatPage /> : <Navigate to={"/login"} />} />
           <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -11,7 +11,7 @@ function ChatPage() {
   const { activeTab, selectedUser } = useChatStore();
 
   return (
-    <div className="relative w-full max-w-6xl h-[calc(100vh-2rem)] mx-auto p-2 sm:p-4 flex flex-col">
+    <div className="relative w-full max-w-6xl h-full mx-auto p-2 sm:p-4">
       <BorderAnimatedContainer>
         {/* LEFT SIDE */}
         <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col border-r border-slate-700/50">
